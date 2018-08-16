efd5a94835c65d401674a2132add2317b8efa893
s3683103 <41530554+s3683103@users.noreply.github.com>
2018-08-16 23:46:42 +1000

    Update products.php

--- a3/products.php
+++ a3/products.php
@@ -41,7 +41,7 @@
 	$menuLength = count($records);
 	echo "<p>{$headings[2]} {$headings[3]} {$headings[4]} {$headings[5]}</p>";
 	for ($x = 0; $x < $records;  $x++) {
-	    print implode(",",$records);
+	    print implode(",", $records[$x]);
 		if ($records = 6 OR $records = 12 OR $records = 18 OR $records = 24){
 			echo "<br>";
 		}
