51b1dbadf3e87636da0e87cdf2d6e34684d80065
s3683103 <41530554+s3683103@users.noreply.github.com>
2018-08-16 17:59:25 +1000

    Update login.php

--- a3/login.php
+++ a3/login.php
@@ -3,15 +3,7 @@
 <head>
     <title>Khon's Asian Takeaway | Login</title>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!--	!--<link href="slicknav.min.css" rel="stylesheet" type="text/css">-->
-<!--	<link href="responsive.css" rel="stylesheet" type="text/css">-->
-	<link href="style.css" rel="stylesheet" type="text/css">
-	<script src="https://use.fontawesome.com/72e868d4a3.js"></script>
-	<script 	  src="https://code.jquery.com/jquery-3.1.1.min.js"
-			  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-			  crossorigin="anonymous"></script>
-	<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+	<link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
 	
 </head>
 <body>
